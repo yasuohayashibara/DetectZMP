fa175cd3ab6c33adaf4586f4074124d60d188f15
yasuohayashibara <yasuo.hayashibara@it-chiba.ac.jp>
2022-03-31 12:43:38 +0900

    initial commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナ変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナで生成されたコード
+
+        /// <summary>
+        /// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディタで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(366, 350);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 350);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        float[] force = new float[4];
+        float[] force0 = new float[4];
+        bool isUpdate = false;
+        bool isFirst = true;
+        string data;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            if (!serialPort1.IsOpen) return;
+            try
+            {
+                data += serialPort1.ReadExisting();
+                var lines = data.Split('\r');
+                int index = data.LastIndexOf("\n");
+                if (index != -1)
+                {
+                    data = data.Substring(index + 1);
+                }
+                var values = lines[0].Split(',');
+                if (values.Count() != 4) return;
+                for(var i = 0; i < 4; i ++)
+                {
+                    force[i] = float.Parse(values[i]);
+                }
+                if (isFirst)
+                {
+                    for (var i = 0; i < 4; i++) force0[i] = force[i];
+                    isFirst = false;
+                }
+                isUpdate = true;
+            }
+            catch (Exception) { }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!isUpdate) return;
+            Bitmap canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(canvas);
+
+            Pen p = new Pen(Color.Black, 2);
+
+            float f0 = force[0] - force0[0];
+            float f1 = force[1] - force0[1];
+            float f2 = force[2] - force0[2];
+            float f3 = force[3] - force0[3];
+            float fa = f0 + f1 + f2 + f3;
+
+            float gx = (-f0 + f1 + f2 - f3) / fa;
+            float gy = ( f0 + f1 - f2 - f3) / fa;
+
+            if (fa > 0.001)
+            {
+                int px = (int)((gx + 2) * pictureBox1.Width / 4);
+                int py = (int)((gy + 2) * pictureBox1.Height / 4);
+                g.DrawEllipse(p, px, py, 20, 20);
+            }
+
+            p.Dispose();
+            g.Dispose();
+
+            pictureBox1.Image = canvas;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            serialPort1.Open();
+        }
+    }
+}
